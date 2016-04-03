12c68e7f384d2206972710396dc487dec7d41d05
mrobakowski <mrobakowski@networkedassets.org>
2016-04-03 22:29:17 +0200

    Change a bunch of types

--- src/main/kotlin/csp.kt
+++ src/main/kotlin/csp.kt
@@ -1,28 +1,46 @@
 import java.util.*
 
-data class Variable<L, V>(val label: L, var value: V? = null, val domain: MutableSet<V>)
+data class Variable<L, V>(val label: L, var value: V? = null) {
+    override fun equals(other: Any?): Boolean {
+        return other != null && other is Variable<*, *> && label == other.label
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hashCode(label)
+    }
+}
+
+data class Constraint<L, V>(
+        val varA: Variable<L, V>,
+        val varB: Variable<L, V>,
+        val constraintPred: (Variable<L, V>, Variable<L, V>) -> Boolean
+) {
+    fun isSatisfied() = constraintPred(varA, varB)
+}
+
 
 class CspSolver<L, V> {
-    var numVariables = 0
-    var variables: MutableList<Variable<L, V>> = ArrayList()
+    var variables: Set<Variable<L, V>> = hashSetOf()
+    var domain: Set<V> = hashSetOf()
+    var constraints: Set<Constraint<L, V>> = hashSetOf()
 
-    private var domainShrinker: CspSolver<L, V>.() -> Unit = {}
+    private var domainShrinker: CspSolver<L, V>.() -> Unit = {} // TODO: better default?
     fun shrinkDomains(shrinker: CspSolver<L, V>.() -> Unit) {
         domainShrinker = shrinker
     }
 
-    private var variableChooser: (List<Variable<L, V>>) -> Variable<L, V> = { it.first() }
-    fun chooseVariable(chooser: (List<Variable<L, V>>) -> Variable<L, V>) {
+    private var variableChooser: (Set<Variable<L, V>>) -> Variable<L, V> = { it.first() }
+    fun chooseVariable(chooser: (Set<Variable<L, V>>) -> Variable<L, V>) {
         variableChooser = chooser
     }
 
-    private var valueChooser: (List<V>) -> V = { it.first() }
-    fun chooseValue(chooser: (List<V>) -> V) {
+    private var valueChooser: (Set<V>) -> V = { it.first() }
+    fun chooseValue(chooser: (Set<V>) -> V) {
         valueChooser = chooser
     }
 
     fun solve(): MutableList<Variable<L, V>>? {
-
+        // TODO: implement
 
         return null
     }
