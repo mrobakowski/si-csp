1efa97ead8dfc4d04bd9ef352b6176aa3e308cb3
Mikołaj Robakowski <mrobakowski@networkedassets.org>
2016-04-04 12:26:48 +0200

    Implement stuff

--- src/main/kotlin/csp.kt
+++ src/main/kotlin/csp.kt
@@ -1,49 +1,84 @@
-import java.util.*
-
-data class Variable<L, V>(val label: L, var value: V? = null) {
-    override fun equals(other: Any?): Boolean {
-        return other != null && other is Variable<*, *> && label == other.label
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hashCode(label)
-    }
-}
-
-data class Constraint<L, V>(
-        val varA: Variable<L, V>,
-        val varB: Variable<L, V>,
-        val constraintPred: (Variable<L, V>, Variable<L, V>) -> Boolean
+data class Variable<L : Any>(val label: L)
+data class Binding<L : Any, V : Any>(val variable: Variable<L>, val value: V)
+data class Constraint<L : Any, V : Any>(
+        val varA: Variable<L>,
+        val varB: Variable<L>,
+        val constraintPred: (Binding<L, V>, Binding<L, V>) -> Boolean
 ) {
-    fun isSatisfied() = constraintPred(varA, varB)
+    fun isSatisfied(bindings: Map<Variable<L>, Binding<L, V>>): Boolean {
+        return constraintPred(bindings[varA] ?: return true, bindings[varB] ?: return true)
+    }
 }
 
-
-class CspSolver<L, V> {
-    var variables: Set<Variable<L, V>> = hashSetOf()
+class CspSolver<L : Any, V : Any> {
+    var variables: Set<Variable<L>> = hashSetOf()
     var domain: Set<V> = hashSetOf()
     var constraints: Set<Constraint<L, V>> = hashSetOf()
 
-    private var domainShrinker: CspSolver<L, V>.() -> Unit = {} // TODO: better default?
-    fun shrinkDomains(shrinker: CspSolver<L, V>.() -> Unit) {
+    private var _initialBindings: MutableMap<Variable<L>, Binding<L, V>> = hashMapOf()
+    var initialBindings: Set<Binding<L, V>>
+        get() = _initialBindings.values.toSet()
+        set(value) {  value.map { it.variable to it }.toMap(_initialBindings) }
+
+    private val _constraintMap: MutableMap<Variable<L>, Set<Constraint<L, V>>> = hashMapOf()
+    interface ConstraintMap<L : Any, V : Any> {
+        operator fun get(v: Variable<L>): Set<Constraint<L, V>>
+    }
+    val constraintMap = object : ConstraintMap<L, V> {
+        override fun get(v: Variable<L>): Set<Constraint<L, V>> = if (v in _constraintMap) {
+            _constraintMap[v]!!
+        } else {
+            _constraintMap[v] = constraints.filter { it.varA == v || it.varB == v }.toSet()
+            _constraintMap[v]!!
+        }
+    }
+
+    private var domainShrinker: CspSolver<L, V>.(Binding<L, V>, Map<Variable<L>, Set<V>>) -> Map<Variable<L>, Set<V>> =
+            { binding, domains -> domains.filterKeys { it != binding.variable } }
+
+    fun shrinkDomains(shrinker: CspSolver<L, V>.(Binding<L, V>, Map<Variable<L>, Set<V>>) -> Map<Variable<L>, Set<V>>) {
         domainShrinker = shrinker
     }
 
-    private var variableChooser: (Set<Variable<L, V>>) -> Variable<L, V> = { it.first() }
-    fun chooseVariable(chooser: (Set<Variable<L, V>>) -> Variable<L, V>) {
+    private var variableChooser: CspSolver<L, V>.(Map<Variable<L>, Binding<L, V>>, Set<Variable<L>>) -> Variable<L> =
+            { bound, unbound -> unbound.first() }
+
+    fun chooseVariable(chooser: CspSolver<L, V>.(Map<Variable<L>, Binding<L, V>>, Set<Variable<L>>) -> Variable<L>) {
         variableChooser = chooser
     }
 
-    private var valueChooser: (Set<V>) -> V = { it.first() }
-    fun chooseValue(chooser: (Set<V>) -> V) {
+    private var valueChooser: CspSolver<L, V>.(Map<Variable<L>, Binding<L, V>>, Variable<L>, Set<V>) -> V =
+            { existingBindings, variable, domain -> domain.first() }
+
+    fun chooseValue(chooser: CspSolver<L, V>.(Map<Variable<L>, Binding<L, V>>, Variable<L>, Set<V>) -> V) {
         valueChooser = chooser
     }
 
-    fun solve(): MutableList<Variable<L, V>>? {
-        // TODO: implement
+    fun solve(): Set<Binding<L, V>>? {
+        val unboundVariables = variables.filter { it !in _initialBindings }.toSet()
+        return solve(_initialBindings, unboundVariables, unboundVariables.map {it to domain}.toMap())
+    }
 
+    private fun solve(
+            bindings: Map<Variable<L>, Binding<L, V>>,
+            unboundVariables: Set<Variable<L>>,
+            domains: Map<Variable<L>, Set<V>>): Set<Binding<L, V>>? {
+        val variableToBind = this.variableChooser(bindings, unboundVariables)
+        val domain = domains[variableToBind].let { if (it == null || it.isEmpty()) return null; it.toMutableSet() }
+        val unboundVariablesWithoutCurrent = unboundVariables - variableToBind
+        while (domain.isNotEmpty()) {
+            val value = this.valueChooser(bindings, variableToBind, domain)
+            domain -= value
+            val binding = Binding(variableToBind, value)
+            val bindingsWithCurrent = bindings + (variableToBind to binding)
+            if (constraintMap[variableToBind].all { it.isSatisfied(bindingsWithCurrent) }) {
+                val shrunkDomains = this.domainShrinker(binding, domains)
+                val solution = solve(bindingsWithCurrent, unboundVariablesWithoutCurrent, shrunkDomains)
+                if (solution != null) return solution
+            }
+        }
         return null
     }
 }
 
-fun <L, V> solver(init: CspSolver<L, V>.() -> Unit) = CspSolver<L, V>().apply(init)
+fun <L : Any, V : Any> solver(init: CspSolver<L, V>.() -> Unit) = CspSolver<L, V>().apply(init)
