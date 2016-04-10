638378a8a1741bba09ec31bff7e7b5bb8f9ee912
mrobakowski <mrobakowski@networkedassets.org>
2016-04-11 00:07:36 +0200

    Start implementing sudoku

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
-
 import javafx.event.EventHandler
 import javafx.geometry.Insets
+import javafx.scene.control.TextField
 import javafx.scene.input.MouseEvent
 import javafx.scene.layout.*
 import javafx.scene.paint.Color
@@ -12,73 +12,134 @@ class HelloWorldApp : App() {
 
 class HelloWorldView : View() {
     override val root = VBox()
+
     private var _checkerboard: GridPane by singleAssign()
     val checkerboard: GridPane get() = _checkerboard
     private val tiles: MutableList<Pane> = arrayListOf()
-
-    var _dim = 0
-
-    var dim: Int
-        get() = _dim
+    private var _dimQueens = 0
+    var dimQueens: Int
+        get() = _dimQueens
         set(value) {
-            if (value < 0) throw IllegalArgumentException("dim")
+            if (value < 0) throw IllegalArgumentException("dimQueens")
 
             checkerboard.children.clear()
             tiles.clear()
             val width = 500.0 / value
             val height = width
 
-            for (y in 0..value-1) {
-                for (x in 0..value-1) {
-                    val color = if (x%2 xor y%2 == 0) Color.BLACK else Color.WHITE
+            for (y in 0..value - 1) {
+                for (x in 0..value - 1) {
+                    val color = if (x % 2 xor y % 2 == 0) Color.BLACK else Color.WHITE
                     val tile = Pane().apply {
                         background = backgroundWithColor(color)
                         prefWidth = width
                         prefHeight = height
-//                        textFill = Color.RED
-//                        alignment = Pos.CENTER
-//                        font = Font.font(font.family, FontWeight.BOLD, 20.0)
                     }
                     tiles += tile
                     checkerboard.add(tile, x, y)
                 }
             }
-            _dim = value
+            _dimQueens = value
+        }
+
+    private var _sudoku: GridPane by singleAssign()
+    val sudoku: GridPane get() = _sudoku
+    private val sudokuCells: MutableList<TextField> = arrayListOf()
+    private var _dimSudoku = 0
+    var dimSudoku: Int
+        get() = _dimSudoku
+        set(value) {
+            if (value < 0) throw IllegalArgumentException("dimSudoku")
+
+            sudoku.children.clear()
+            sudokuCells.clear()
+            val width = 500.0 / value
+            val height = width
+
+            for (y in 0..value - 1) {
+                for (x in 0..value - 1) {
+                    val textField = TextField().apply {
+                        prefWidth = width
+                        prefHeight = height
+                    }
+                    sudokuCells += textField
+                    sudoku.add(textField, x, y)
+                }
+            }
+            _dimSudoku = value
         }
 
     private fun backgroundWithColor(color: Color) = Background(BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY))
 
     init {
         title = "CSP"
-        var topRow: HBox? = null
+        var topRowQueens: HBox? = null
+        var topRowSudoku: HBox? = null
 
         with(root) {
-            topRow = hbox {
-                val dimens = textfield {
-                    hboxConstraints { margin = Insets(5.0); }
-                }
-                button("Generate!") {
-                    hboxConstraints { margin = Insets(5.0) }
-                    onMouseClicked = EventHandler<MouseEvent> {
-                        dim = dimens.text.toInt()
-                        val progInd = topRow?.progressIndicator()
+            tabpane {
+                tab("n-Queens", VBox()) {
+                    topRowQueens = hbox {
+                        val dimens = textfield {
+                            hboxConstraints { margin = Insets(5.0); }
+                        }
+                        button("Generate!") {
+                            hboxConstraints { margin = Insets(5.0) }
+                            onMouseClicked = javafx.event.EventHandler<MouseEvent> {
+                                dimQueens = dimens.text.toInt()
+                                val progInd = topRowQueens?.progressIndicator { prefHeight = 5.0 }
 
-                        background {
-                            nQueens(dim, printToStd = false)
-                        } ui {
-                            if (it != null) {
-                                for ((x, y) in it) {
-                                    tiles[(y - 1) * dim + (x - 1)].background = backgroundWithColor(Color.RED)
+                                background {
+                                    nQueens(dimQueens, printToStd = false)
+                                } ui {
+                                    if (it != null) {
+                                        for ((x, y) in it) {
+                                            tiles[(y - 1) * dimQueens + (x - 1)].background = backgroundWithColor(javafx.scene.paint.Color.RED)
+                                        }
+                                    }
+                                    topRowQueens?.children?.remove(progInd)
                                 }
                             }
-                            topRow?.children?.remove(progInd)
                         }
                     }
-                }
+                    _checkerboard = gridpane()
+                }.apply { isClosable = false }
+
+                tab("Sudoku", VBox()) {
+                    topRowSudoku = hbox {
+                        val dimens = textfield {
+                            hboxConstraints { margin = Insets(5.0); }
+                        }
+                        button("Solve") {
+                            hboxConstraints { margin = Insets(5.0) }
+                            onMouseClicked = javafx.event.EventHandler<MouseEvent> {
+                                dimSudoku = dimens.text.toInt().let { it * it }
+                                val progInd = topRowSudoku?.progressIndicator { prefHeight = 5.0 }
+
+                                background {
+                                    sudoku(dimSudoku, mapOf())
+                                } ui {
+                                    if (it != null) {
+                                        for (i in it) {
+
+                                        }
+                                    }
+                                    topRowSudoku?.children?.remove(progInd)
+                                }
+                            }
+                        }
+                    }
+                    _sudoku = gridpane()
+                }.apply { isClosable = false }
             }
-            _checkerboard = gridpane()
+
         }
 
-        dim = 10
+        dimQueens = 9
+        dimSudoku = 9
+
+//        primaryStage.isResizable = false
+//        primaryStage.width = 500.0
+//        primaryStage.height = 597.0
     }
 }
---
+++ src/main/kotlin/Sudoku.kt
@@ -0,0 +1,8 @@
+fun sudoku(dim: Int, initialCond: Map<Pair<Int, Int>, Int>): List<Int>? {
+    val s = solver<Pair<Int, Int>, Int> {
+        val domain = 1..dim
+    }
+
+
+    return null
+}
